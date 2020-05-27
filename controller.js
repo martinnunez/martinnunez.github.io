cfb799fa6832355a860d1d30c45598b7bbcbe247
Diego Martín Núñez Guzmán <nunez.guzman@gmail.com>
2020-05-26 22:54:31 -0500

    open in same tab

--- controller.js
+++ controller.js
@@ -77,9 +77,9 @@ function callModal() {
 
 function giveFileName() {
     let element = document.getElementById('company-txt').value;
-    let a = doc.save('cotización-' + element + getDateTime());
-    var x = window.open();
-    x.document.open();
+    doc.save('cotización-' + element + getDateTime());
+    // var win = window.open();
+    // win.document.write('<iframe src="' + doc.output('datauri')  + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
 }
 
 function getDateTime() {
